0dc6fc9214bf51b64bc8fd7529f1c3e8beadcb68
Serhat Sel <selserhat01@gmail.com>
2024-03-14 03:07:53 +0300

    test with mock

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -16,4 +16,21 @@ describe("Async components testing", () => {
     expect(listsItemElements).toHaveLength(100);
     //! expect(listsItemElements).toHaveLength(111);
   });
+
+  //? Mock style
+  test("renders posts id request succeeds (MOCK)", async () => {
+    //Arrange
+
+    render(<Async />);
+    global.fetch = jest.fn().mockResolvedValueOnce({
+      json: async () => [{ id: "1", title: "mocked data" }],
+    });
+
+    //Act
+
+    //Assert
+
+    const listsItemElements = await screen.findAllByRole("listitem");
+    expect(listsItemElements).not.toHaveLength(0);
+  });
 });
